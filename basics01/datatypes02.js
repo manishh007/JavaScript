c0e0197c9e396bde79cb44175572c6875d0b591f
Manish Pratap Singh <123986689+manishh007@users.noreply.github.com>
2024-09-12 18:45:37 +0000

    conversion of datatypes

---
+++ basics01/datatypes02.js
@@ -0,0 +1,41 @@
+let score = false
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+//console.log(score);
+
+
+
+/* To convert variables
+"20" -> 20
+"20abc" -> gives Nan matlab undefined
+*/
+
+let isLoggedIn = "0"
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+//console.log(isLoggedIn);
+
+//console.log(booleanIsloggedIn);
+
+/* for conversion into boolean 
+0 -> false
+1 -> true 
+12 -> true
+"" -> false
+"1" -> true , matlab quotes ke andar kuch bhi ho except empty
+*/
+
+let someNumber = 12
+let numberToString = String(someNumber)
+console.log(numberToString);
+
+console.log(typeof numberToString);
+
+console.log(typeof someNumber);
+
