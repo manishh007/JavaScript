e6bf5faa0a50cd8aa19bd342d9fa9e609bcb4ac9
Manish Pratap Singh <123986689+manishh007@users.noreply.github.com>
2024-09-12 18:45:07 +0000

    study of variables

---
+++ basics01/variables01.js
@@ -0,0 +1,19 @@
+const accountId = 225
+let accountEmail = "hello@gmail.com"
+var accountPassword = "112233"
+accountCity = "Bulandshahr"
+
+// accountId = 12 // not allowed
+
+console.log(accountId);
+
+accountEmail = "akku@gmail.com"
+accountPassword = "1234"
+accountCity = "ghaziabad"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/* never to use var because it has problem with scope */
+
+
+
